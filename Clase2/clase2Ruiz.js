1576b64affae8e4a9a4710d20ae107422958ac5a
Victor Ruiz <ruizevictor@gmail.com>
2021-09-29 19:57:00 -0300

    mensaje concatenado

--- Clase2/clase2Ruiz.js
+++ Clase2/clase2Ruiz.js
@@ -1,7 +1,7 @@
 let numero = parseInt(prompt("Ingrese un numero para determinar si es par o impar"));
 
 if (numero % 2 == 0){
-    console.log("El numero es par")
+    console.log("El numero "+ numero + " es par")
 }else if( numero % 2 != 0 && Number.isInteger(numero) == true){
-    console.log("El numero es impar ")
+    console.log("El numero "+ numero +" es impar ")
 }else {console.log("Dato ingresado no es valido")}
