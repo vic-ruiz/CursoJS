3986e51ad3d4402633cc0879e83dc49074d7d378
Victor Ruiz <ruizevictor@gmail.com>
2021-09-29 20:42:56 -0300

    entrega

--- Clase2/clase2Ruiz.js
+++ Clase2/clase2Ruiz.js
@@ -1,7 +1,7 @@
-let numero = parseInt(prompt("Ingrese un numero para determinar si es par o impar"));
+let numero = parseInt(prompt("Ingrese un numero entero para determinar si es par o impar"));
 
 if (numero % 2 == 0){
     alert("El numero "+ numero + " es par")
-}else if( numero % 2 != 0 && Number.isInteger(numero) == true){
+}else if(Number.isInteger(numero) == true){
     alert("El numero "+ numero +" es impar ")
 }else {alert("Dato ingresado no es valido")}
