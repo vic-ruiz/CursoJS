bc97c0aa93ca6c8aa8ca0d15ea71b224bfb8adff
Victor Ruiz <ruizevictor@gmail.com>
2021-10-05 11:18:56 -0300

    clase3

---
+++ Clase3/clase3Ruiz.js
@@ -0,0 +1,13 @@
+let opcion = " "
+while(opcion!="ESC"){
+    let numero = parseInt(prompt("Ingrese un numero y obtenga sus primeros 5 multiplos"));
+    for (let i = 1; i <= 5 ; i++){
+        alert(numero + " x " + i + " es igual a " + (i*numero))
+    }
+
+    opcion = prompt("Ingrese: ESC Para detener el programa \nIngrese cualquier otro caracter para volver a ejecutar ").toUpperCase()
+    /* toUpperCase por si el usuario ingresa esc en lowercase */
+
+}
+
+
