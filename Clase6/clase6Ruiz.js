3e3ba73cffe580ced20afb30cb4659b9abcaf85a
Victor Ruiz <ruizevictor@gmail.com>
2021-10-16 13:23:26 +0200

    entrega 6

--- Clase6/clase6Ruiz.js
+++ Clase6/clase6Ruiz.js
@@ -3,22 +3,23 @@ var precio = 0;
 var cantidad = 0;
 
 class Pelicula {
-    constructor(titulo, estreno, duracion, id) {
+    constructor(titulo, estreno, duracion, id, precio) {
         this.titulo = titulo;
         this.estreno = estreno;
         this.duracion = duracion;
         this.id = id
+        this.precio = precio
     }
 
     descripcion(){
-        alert("Se selecciono la pelicula "+this.titulo+" estrenada en "+this.estreno+", con una duracion de "+this.duracion+" minutos");
+        alert("Se selecciono la pelicula "+this.titulo+" estrenada en "+this.estreno+", con una duracion de "+this.duracion+" minutos \nCon un precio de "+this.precio+" pesos por entrada");
     }
 
 }
 
-const pelicula1 = new Pelicula("Kimetsu no Yaiba: Mugen Ressha-hen",2020,117,1);
-const pelicula2 = new Pelicula("Akira",1988,124,2);
-const pelicula3 = new Pelicula("El viaje de Chihiro",2001,120,3);
+const pelicula1 = new Pelicula("Kimetsu no Yaiba: Mugen Ressha-hen",2020,117,1,600);
+const pelicula2 = new Pelicula("Akira",1988,124,2,700);
+const pelicula3 = new Pelicula("El viaje de Chihiro",2001,120,3,800);
 
 const catalogo = [pelicula1, pelicula2, pelicula3]
 
@@ -34,16 +35,16 @@ function seleccionarCantidad(){
     return parseInt(prompt("Indique la cantidad de entradas que desea comprar"));
 }
 
-function precioOrden(){
+function precioOrden(a){
     cantidad = seleccionarCantidad();
-    return total += (500*cantidad);
+    return total += (a*cantidad);
 }
 
 do{
     mostrarCatalogo();
     let producto = parseInt(prompt("Seleccione a que pelicula desea ver de acuerdo a la enumeracion del catalogo"));
     catalogo.find(element => element.id === producto).descripcion();
-    precioOrden();
+    precioOrden(catalogo.find(element => element.id === producto).precio);
     otroProducto = confirm("¿Quiere reservar mas entradas?")
 } while(otroProducto);
 
